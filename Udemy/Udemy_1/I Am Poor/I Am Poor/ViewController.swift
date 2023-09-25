7db3ce4b6d8a045f2fa12a98ddff7a944db1c36d
WooHyun <woohyun@WooHyuns-MacBook-Pro.local>
2023-09-26 01:46:32 +0900

    udemy 2주차

---
+++ Udemy/Udemy_1/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by WooHyun on 2023/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
