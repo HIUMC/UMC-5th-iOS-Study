033c82811796801fc7968466dc919920dae3f018
W99Hyun <jwh990727@naver.com>
2023-10-06 18:11:45 +0900

    Feat: 재설정
    재설정

---
+++ W99Hyun/Week_1/before/enurytime/InsetTextField.swift
@@ -0,0 +1,31 @@
+//
+//  InsetTextField.swift
+//  enurytime
+//
+//  Created by WooHyun on 2023/09/18.
+//
+
+import Foundation
+import UIKit
+
+class InsetTextField : UITextField {
+    var insetX: CGFloat = 0 {
+        didSet {
+            layoutIfNeeded()
+        }
+    }
+    
+    var insetY: CGFloat = 0 {
+        didSet {
+            layoutIfNeeded()
+        }
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: insetX, dy: insetY)
+    }
+
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: insetX, dy: insetY)
+    }
+}
