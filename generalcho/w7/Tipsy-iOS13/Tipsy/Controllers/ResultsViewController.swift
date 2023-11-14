e9eb8d3c89dd6752dc258ccce51a527b85bf1493
(soungjunban) <(soungjunban@gmail.com)>
2023-11-14 16:16:45 +0900

    (Feat)generalban-w6

---
+++ generalcho/w7/Tipsy-iOS13/Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    var result = "0.0"
+    var tip = 10
+    var split = 2
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        totalLabel.text = result
+        settingsLabel.text = "Split between \(split) people, with \(tip)% tip."
+
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+}
