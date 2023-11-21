83595585f8fa43524b61430e4863a46371dd8f47
Seyoung <seyoung@30Mackbookair.local>
2023-11-21 16:31:58 +0900

    [Feat(30isdead):Week 8]

---
+++ 30/Week8/Catstagram_W8/Pods/Target Support Files/Pods-Catstagram_W7-Catstagram_W7UITests/Pods-Catstagram_W7-Catstagram_W7UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Catstagram_W7_Catstagram_W7UITests : NSObject
+@end
+@implementation PodsDummy_Pods_Catstagram_W7_Catstagram_W7UITests
+@end
---
+++ 30/Week8/Catstagram_W8/Pods/Target Support Files/Pods-Catstagram_W7Tests/Pods-Catstagram_W7Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Catstagram_W7Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Catstagram_W7Tests
+@end
